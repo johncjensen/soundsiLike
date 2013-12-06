f2dcd8706c2c0f59f2db0a590e42d82ea3652079
John Jensen <johncjensen@hotmail.com>
2013-12-05 20:40:08 -0800

    css compressor on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Soundsilike::Application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+   config.assets.css_compressor = :sass
   config.assets.js_compressor = Uglifier.new(:mangle => false)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
