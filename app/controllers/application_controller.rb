f387fa9f2e713ffe22aa5104fa8e524cac7ada00
John Jensen MBP <johncjensen@hotmail.com>
2014-03-27 02:09:53 -0700

    add helper method to set genre within soundcloud pull & DRY the sh*t out of the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  helper_method :set_genre_to_pull
+
+  def set_genre_to_pull(value)
+    @client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_CLIENT_ID'])
+    @tracks = @client.get('/tracks', :limit => 21, :tags => "#{value}", :filter => 'downloadable', :order => 'hotness')
+    @hotmusic = @tracks
+    @downloads = ''
+  end
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
