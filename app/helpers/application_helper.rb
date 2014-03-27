7340c5fbfa744e2cc7d9b04be52f6b1365e4eefc
John Jensen MBP <johncjensen@hotmail.com>
2014-03-27 02:25:50 -0700

    add helper to check_if_artwork_present and use placehold.it to display something if nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+  def check_if_artwork_present(value)
+    if value.artwork_url.nil?
+      "http://placehold.it/600x300/2D313D/f6f6f6&text=no+artwork"
+    else
+      value.artwork_url.to_s.gsub('-large', '-t500x500')
+    end
+  end
+
   def bootstrap_class_for flash_type
     case flash_type
     when :success
