ba339deb535fab31a37e72c543240ee7d62e8d5f
John Jensen MBP <johncjensen@hotmail.com>
2014-03-27 01:41:48 -0700

    clean up ugly code to find current user downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -4,8 +4,8 @@ class DownloadsController < ApplicationController
     else
       redirect_to :root
     end
-    @downloads = Download.where user_id: is = current_user.id
-    @realname = User.where user_id: is = current_user.id
+    @downloads = Download.where(:user_id => current_user.id)
+    @realname = User.where(:user_id => current_user.id)
     @shit = ''
   end
 
@@ -24,31 +24,19 @@ class DownloadsController < ApplicationController
     end
   end
 
-  # def create
-  #   @download = Download.create(download_params)
-  #   #do whatever you want
-  #     respond_to do |format|
-  #       if @download.save
-  #         format.js
-  #         format.html { redirect_to(@drum_and_bass, :notice => 'Message was successfully created.') }
-  #       else
-  #         #deal with errors here/ redirect wherever you want
-  #       end
-  #     end
-  #   end
 
 
-    def destroy
-      @download = Download.where user_id: is = current_user.id
-      @download.destroy_all
-      redirect_to downloads_path
-    end
-
-
-    private
-
-    def download_params
-      params.permit(:title, :url, :user_id, :bpm, :genre)
-    end
-
+  def destroy
+    @download = Download.where(:user_id => current_user.id)
+    @download.destroy_all
+    redirect_to downloads_path
   end
+
+
+  private
+
+  def download_params
+    params.permit(:title, :url, :user_id, :bpm, :genre)
+  end
+
+end
